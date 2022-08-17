9ea542434cd6931c8141cc702d2bbb44065f3cb6
Dahee9532 <107235450+Dahee9532@users.noreply.github.com>
2022-08-17 09:25:27 +0900

    Create 2.cpp

---
+++ 11 - 그리디 기출문제/2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main(void){
+    cin >> str;
+    int total = int(str[0]-48);
+    
+    for(int i = 0; i < str.size()-1; i++){
+        //cout << int(str[i]-48) << '\n';
+        if(int(str[i]-48) == 0 || int(str[i]-48) == 1){
+            total += int(str[i+1]-48);
+        }
+        else total *= int(str[i+1]-48);
+    }
+    cout << total << '\n';
+}
