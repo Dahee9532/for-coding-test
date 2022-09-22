38da95aab3c202d93fdc8e1e42e6b38600927369
Dahee9532 <107235450+Dahee9532@users.noreply.github.com>
2022-09-22 12:05:04 +0900

    Update 경쟁적전염.cpp

--- 5 - DFS_BFS 기출문제/경쟁적전염.cpp
+++ 5 - DFS_BFS 기출문제/경쟁적전염.cpp
@@ -2,63 +2,53 @@
 
 using namespace std;
 
-int N, K, S;
-int X, Y;
+int N, K, S, X, Y;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 int graph[200][200];
-bool visit[200][200];
 
-void dfs(int x, int y){
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx < 0 || nx >= N || ny  < 0 || ny >= N || visit[nx][ny] || graph[nx][ny] != 0) continue;
-        visit[nx][ny] = true;
-        graph[nx][ny] = graph[x][y];
-    }
-}
+struct Virus{
+    int val;
+    int x;
+    int y;
+};
 
-void visitSetting(){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            visit[i][j] = false;
-        }
-    }
+bool cmp(Virus& v1, Virus& v2){
+    return v1.val < v2.val;
 }
 
 int main(void){
-    cin >> N >> K; // N x N, 1부터 K까지 바이러스 종류
+    cin >> N >> K;
+    vector<Virus> vec;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cin >> graph[i][j];
+            if(graph[i][j] >= 1 && graph[i][j] <= K) vec.push_back({graph[i][j], i, j});
         }
     }
-    cin >> S >> X >> Y;// 2 3 2 -> 2초 후 (3,2)에 위치하는 바이러스 종류
+    cin >> S >> X >> Y;
+    sort(vec.begin(), vec.end(), cmp);
+    int s = 0;
 
-    // for(int i = 0; i < N; i++){
-    //     for(int j = 0; j < N; j++){
-    //         cout << graph[i][j] << ' ';
-    //     }
-    //     cout << '\n';
-    // }
-
-    for(int i = 0; i < S; i++){
-        visitSetting();
-        for(int j = 0; j < K; j++){//1부터 적용하기 ~ K까지
-            for(int k = 0; k < N; k++){
-                for(int l = 0; l < N; l++){
-                    if(graph[k][l] == (j+1) && !visit[k][l]) dfs(k, l);
+    while(s < S){
+        int len = vec.size();
+        for(int i = 0; i < len; i++){
+          Virus v = vec[i];
+            for(int j = 0; j < 4; j++){
+                int nx = v.x + dx[j];
+                int ny = v.y + dy[j];
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                if(!graph[nx][ny]){
+                    graph[nx][ny] = v.val;
+                    vec.push_back({graph[nx][ny], nx, ny});
                 }
             }
         }
+        if(graph[X-1][Y-1] > 0) break;
+        s++;
     }
-    // cout << '\n';
-    // for(int i = 0; i < N; i++){
-    //     for(int j = 0; j < N; j++){
-    //         cout << graph[i][j] << ' ';
-    //     }
-    //     cout << '\n';
-    // }
     cout << graph[X-1][Y-1] << '\n';
+
 }
+
+//https://iot624.tistory.com/147 
