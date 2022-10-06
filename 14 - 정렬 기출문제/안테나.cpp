6bc3926b406bebf7e9c536ee413d23195ede737b
Dahee9532 <107235450+Dahee9532@users.noreply.github.com>
2022-10-06 21:35:24 +0900

    Update 안테나.cpp

--- 14 - 정렬 기출문제/안테나.cpp
+++ 14 - 정렬 기출문제/안테나.cpp
@@ -7,11 +7,13 @@ vector<int> v;
 
 int main(void){
   cin >> n;
+  
   for(int i = 0; i < n; i++){
     int x;
     cin >> x;
     v.push_back(x);
   }
+  
   sort(v.begin(), v.end());
 
   cout << v[(n-1)/2] << '\n';
